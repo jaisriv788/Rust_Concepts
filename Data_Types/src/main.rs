1b0b4e9a0f5382b316b5c55eada4e96c5e366362
jaisriv788 <srivastavajai845@gmail.com>
2024-12-18 11:09:34 +0530

    addtional data

--- Data_Types/src/main.rs
+++ Data_Types/src/main.rs
@@ -1,3 +1,7 @@
+// `#[allow(unused_variables)]` is a compiler directive in Rust that suppresses warnings related to
+// unused variables. This attribute tells the Rust compiler to ignore warnings that would typically be
+// generated when variables are declared but not used in the code. It can be helpful during development
+// to avoid unnecessary noise from the compiler about variables that are intentionally left unused.
 #[allow(unused_variables)]
 fn main() {
     //Below are the decalration for the data types.
@@ -27,6 +31,8 @@ fn main() {
     let pi2: f64 = 22.0 / 7.0;
 
     //to control the number of digits after the decimal we use somthing like below
+    // The code `println!("{pi1:.2}"); println!("{:.3}", pi2);` is formatting and printing the
+    // floating-point numbers `pi1` and `pi2` with a specific number of decimal places.
     println!("{pi1:.2}");
     println!("{:.3}", pi2);
 
@@ -48,4 +54,21 @@ fn main() {
 
     //Array
     let arr: [i32; 6] = [1, 2, 3, 4, 5, 6];
+    //below line is not executable as it does not contain display trait but have a debug trait.
+    // println!("{}",arr);
+
+    // `println!("{:?}", arr);` is a Rust macro that is used to print the debug representation of the
+    // `arr` array. The `{:?}` format specifier is used to print the array in a debug format, which
+    // means it will display the content of the array in a way that is suitable for debugging purposes.
+    // This format specifier is commonly used to print arrays, structs, enums, and other complex data
+    // types for debugging and inspection.
+    println!("{:?}", arr);
+    println!("{arr:?}");
+
+    // `println!("{:#?}", arr);` is a Rust macro that is used to print the debug representation of the
+    // `arr` array with a pretty-printing format. The `{:?}` format specifier is used to print the
+    // array in a debug format, and the `#` symbol modifies the output to be more visually structured
+    // and easier to read. This format specifier is commonly used for debugging purposes to display
+    // complex data types like arrays, structs, enums, etc.
+    println!("{:#?}", arr);
 }
